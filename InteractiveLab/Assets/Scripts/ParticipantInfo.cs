66a605911638ff8631e51bf616d303f8cb7c2e81
Peter <peter.dushniku@gmail.com>
2026-03-13 04:26:27 -0400

    VR Info

--- InteractiveLab/Assets/Scripts/ParticipantInfo.cs
+++ InteractiveLab/Assets/Scripts/ParticipantInfo.cs
@@ -5,28 +5,64 @@ using TMPro;
 
 public class ParticipantInfo : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField ageInput;
-    [SerializeField] private TMP_InputField idInput;
-    [SerializeField] private TMP_Dropdown sexInput;
+    private TMP_Dropdown sexInput;
+
+    public TestMode testMode;
+
+    public TMP_InputField nonVRAgeInput;
+    public TMP_InputField nonVRIdInput;
+    public TMP_Dropdown nonVRSexInput;
+
+    public TMP_Dropdown VRAgeInput;
+    public TMP_Dropdown VRIdInput;
+    public TMP_Dropdown VRSexInput;
 
     [SerializeField] private MouseLook mouseLook;
-    public GameObject infoUI;
+    public GameObject nonVRInfoUI;
+    public GameObject VRInfoUI;
+    private GameObject infoUI;
     public Recording.OutputManager outputManager;
     public Movement movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        if (testMode == TestMode.Screen)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            sexInput = nonVRSexInput;
+            infoUI = nonVRInfoUI;
+            nonVRInfoUI.SetActive(true);
+            VRInfoUI.SetActive(false);
+        }
+        else
+        {
+            sexInput = VRSexInput;
+            infoUI = VRInfoUI;
+            nonVRInfoUI.SetActive(false);
+            VRInfoUI.SetActive(true);
+        }
+
         Time.timeScale = 0f;
     }
 
     public void Submit()
     {
-        string age = ageInput.text;
-        string id = idInput.text;
+        string age;
+        string id;
         string sex = sexInput.options[sexInput.value].text;
+
+        if (testMode == TestMode.Screen)
+        {
+            age = nonVRAgeInput.text;
+            id = nonVRIdInput.text;
+        }
+        else
+        {
+            age = VRAgeInput.options[VRAgeInput.value].text;
+            id = VRIdInput.options[VRIdInput.value].text;
+        }
 
         infoUI.SetActive(false);
         outputManager.Setup(age, id, sex);
